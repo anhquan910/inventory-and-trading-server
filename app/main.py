ac3455bbb3493083c1d7ffdb3a35d152762efdd1
anhquan910 <anh.nguyen.14@city.ac.uk>
2025-12-21 20:51:38 +0700

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.routes import auth, users, inventory
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include Routers
 app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["auth"])
